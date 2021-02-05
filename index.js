778406f22a215afe0198573d02898dd84814a85d
NickJL524 <nicklamothe524@gmail.com>
2021-02-04 19:53:11 -0500

    comment out

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ function ask(questionText) {
 start();
 
 async function start() {
-  const welcomeMessage = (`It’s a dark damp night and you swear you heard a soft whisper\n You think maybe that was just in your head?. \nYou need to decide, Should I really continue.`);
-  
+  const welcomeMessage = (`It’s a dark damp night and you swear you heard a soft whisper\n You think maybe that was just in your head?. \nYou need to decide, Should you really continue.`);
+  //while(answer !== 'exit') {
+ //answer = await ask('>_ ')
+//}
+  //console.log("I don't know how to do _____");
+//let sign = {
+  //description: "The code to enter is 12345.\nEnter at your own risk. "
+  //read: () => {
+    //return this.description
+  //}
+  //}
   if (welcomeMessage === "yes" || "y")
   console.log ("Lets go.")
   
-  
-  let answer = await ask(welcomeMessage);
   let userName = await ask("What is your name?")
+  let answer = await ask(welcomeMessage);
+  
   
   
   //console.log(` `)
@@ -49,12 +58,11 @@ async function start() {
 
 
 
-//while(answer !== 'exit') {
- //answer = await ask('>_ ')
-//}
+
+
 //let sign = {
   //description: "The code to enter is 12345.\nEnter at your own risk. "
   //read: () => {
     //return this.description
   //}
-//}
+  //}
