6865ef5fd7f9d6fd9761e5de8dbe6573caade510
Jack Brooks <cyrusandethan@yahoo.com>
2021-02-05 12:06:10 -0500

    ceaning my files

--- index.js
+++ index.js
@@ -1,58 +1,69 @@
-const readline = require('readline');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+const { Console } = require("console");
+const readline = require("readline");
+const { start } = require("repl");
+const readlineInterface = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
     readlineInterface.question(questionText, resolve);
   });
 }
+console.log(
+  `It’s a dark damp night and you swear you heard a soft whisper\n You think maybe that was just in your head?. \nYou need to decide\n Should I really continue.`);
 
 start();
 
-async function start() {
-  const welcomeMessage = (`It’s a dark damp night and you swear you heard a soft whisper\n You think maybe that was just in your head?. \nYou need to decide, Should I really continue.`);
-  
-  if (welcomeMessage === "yes" || "y")
-  console.log ("Lets go.")
-  
-  
-  let answer = await ask(welcomeMessage);
-  let userName = await ask("What is your name?")
-  
-  
-  //console.log(` `)
-  console.log(`Hello,  ${userName}, let's get started.`)
-  //process.exit();
+async function start, (return(Promise.resolve)
+  const storyLine = ask("so do you want to play?") )
 }
-class Room {
-  constructor(description, inventory, left, right, straight, back);
-    
-  this.description = description;
-  this.inventory = inventory;
-  this.left = left;
-  this.right = right;
-  this.straight = straight;
-  this.back = back;
+
+start.then({  (storyLine === "yes" || "y") 
+if (storyline === true) {console.log(
+  "Very Good/n There's just a just have a couple of things we have to discuss"
+)}
+
+else {console.log("sorry to take up any of  your time\n Goodbye")}
+  })
+  let answer = await ask();
+  let userName = await ask("What is your name? ");
+
+  console.log(`Hello', ${userName}, 'let us get started.`);
+  //else {console.log ('Sorry to take up any of your time./n enjoy this gorgeous night',(process.exit)}
 }
-describe(){
-  return 
-}
-let myRoom = new Room("")
+//class Room {
 
-console.log(myRoom.describe())
+//constructor(description, inventory, left, right, straight, back);
 
-let orderArr = process.argv.slice(2);
+//  this.description = description;
+//  this.inventory = inventory;
+//  this.left = left;
+//  this.right = right;
+//  this.straight = straight;
+//  this.back = back;
 
-orderArr.forEach((items) => {
-  orderTotal += menu[items];
-});
+//}
+//describe(){
+//return
+//}
+//let myRoom = new Room("")
+
+//console.log(myRoom.describe())
+
+//let orderArr = process.argv.slice(2);
+
+//orderArr.forEach((items) => {
+// orderTotal += menu[items];
+//});
 
 //while(answer !== 'exit') {
- //answer = await ask('>_ ')
+//answer = await ask('>_ ')
 //}
 //let sign = {
-  //description: "The code to enter is 12345.\nEnter at your own risk. "
-  //read: () => {
-    //return this.description
-  //}
+//description: "The code to enter is 12345.\nEnter at your own risk. "
+//read: () => {
+//return this.description
+//}
 //}
