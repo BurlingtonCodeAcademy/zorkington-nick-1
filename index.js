459225241a3165ac9ad60824e961d3accfad5ffa
Jack Brooks <cyrusandethan@yahoo.com>
2021-02-07 06:31:40 -0500

    Later today

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 //this sectoion was goiven
 const { Console } = require("console");
 const readline = require("readline");
-//const { Console } = require("console");
-//const readline = require("readline");
-//const { start } = require("repl");
+const { begin } = require("repl");
 const readlineInterface = readline.createInterface(
   process.stdin,
   process.stdout
 )
-
 start()
-async function start() {
- 
+
+async function start() { 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
     r1.question(questionText), resolve;
   });
 }//end of given (see first note)
+
 //creating rooms and player: classes, groups and arrays; week 2 objects method, slide 2- and OsJs slide 9 = definition 
 
 //Satchel is where you'll find what player has picked up 
@@ -43,13 +41,11 @@ class grabIt{
 //Objects player can pickup but must leave there
 class moveableObjects{
   constructor(spiders)
-  {
-    This.spider=spider
+  {This.spider=spider
   }
 }
 class dropped{constructor(stick, hammer)
-  {
-  this.stick=stick
+  {this.stick=stick
   this.hammer=hammer
   }
 }
@@ -64,7 +60,7 @@ async function start() {
   
   //if yes lets start the game
   if (answer === "yes" || answer === "y")
-    //console.log ("Lets go.")
+  console.log ("Lets go.")
 
     //console.log(` `)
     console.log(`Ok, ${userName}, Let us begin.\nHead for the front door of City Hall right behind you.`
@@ -76,26 +72,31 @@ async function start() {
   
   let sign = {
   description: "The code to enter is 1234.\nEnter at your own risk. "
-  read: () => {
-  return this.description
+  //read: () => {
+  //return this.description
   }
   }
   
-  
-  if (doorCode !== 12345){
+  let doorCode = await ask(description);
+  let count = 0
   console.log("The sign says \"Welcome to City Hall.\n To enter you must input this code ",1,2,3,4," in the, keypad, below.")
-  } 
-  else{while (count<4){console.log("You've had three tries/n You are not serious, go home to mother dear.")}
+  if (doorCode === 12345){console.log("enter")
+    } 
+  else{while (count<4){console.log("You've had three tries/n You are not serious, go home to mother dear."), process.exit}
+  let count = count + 1
   
   //entering first rm
 
-  console.log("You have entered the Antechamber you have three choices where to go.\n To the left is a nice sunny lit hallway with people working in their offices.\n to the right is a dark dreary hallway with cobwebs covering the entrance.\n Straight ahead is a huge marble staircase that has been roped off with a sign that reads AUTHORIZED PERSONNEL ONLY.\n What way will you choose?"/n /n)
-  console.log ("meanwhile, the door you entered has closed and you heard it lock")
-
-  async function amait("left" || "right" || "straight")
+  console.log("You have entered the Antechamber you have three choices where to go.\n To the left is a nice sunny lit hallway with people working in their offices.\n to the right is a dark dreary hallway with cobwebs covering the entrance.\n Straight ahead is a huge marble staircase that has been roped off with a sign that reads AUTHORIZED PERSONNEL ONLY.\n Which way will you choose?/n Left, Right or straight/n Meanwhile, you heard the door lock behind you")
+  //function lockDoor {}
+  
+direction; amait.ask("left, right or straight")
   let (left === brightHall, right === darkHall, straight === stairs)
-if left ()
-//creating rooms and player: week
+if (left) {console.log("you are standing at the entrance to the brigthly lit hallway/n you hear people workingbut you remember/n It was dark outside, so where is the sun doming from?, /n DO YOU ENTER/n Yes or No?")}
+
+
+
+//creating rooms and player: 
 class Satchel{ 
   constructor(weapon1){
      this.weapon1 = weapon1;
@@ -154,144 +155,11 @@ console.log("welcome to Nick's Jack's Likely Unfinished Game\n \n");
 
 console.log("Have the Time Of Your Life!!");
 
-//The game starts now with the opening scene,
-async function start() {
-  const welcomeMessage = `It’s a dark damp night and you are on the road out in front of 182 Main St. and you swear you heard a soft whisper.\nWould you like to play a game?\nYou think maybe that was just in your head. \nYou need to decide, Do you really want to continue.?n\ONLY respond with a Yes or a No`;
-  console.log(welcomeMessage)
-  //The potentioal player needs to respond with a Y or N to move on
-  //The response is evaluated to determine if they move on
-  process.stdin.once("data", (yesOrNo) => {let yesOrNo = "opening".toLowerCase. trim(1)
-  });
-  
-  if (opening = y||n) {
-  return}
-  else {console.log("Please respond with a Y or N");
-
-   if (yesOrNo == "yes" || "y" || "Y") {return}
-  else {console.log ('Please respond with a Y or N')}
-    let userName = await ask("What is your name?") //we wait for their 2 responses
-  if (yesOrNo == "yes" || "y" || "Y") {
-    return;
-  } else {
-    comsole.log("Please respond with a Y or N");
+let anteChamber = ["stairCase", "darkHall", "brightHall", "sign"]
+;
+    
+let study = ["books", "spiders", "redCarpet", "door", "door"];
+    let sachel = [""]
+ 
   }
-  let userName = await ask("What is your name?"); //we wait for their response
-  Let (answer) = player
-
-console.log ('See the entrance door to City Hall?\n There is a note there, read that note and respond Immediately\n You will have three three (3!) tries to get it correctly\n \n \nThe note sitting on a key reads\n  \n  \nWelcome' ${answer}, 'the code is 12345');
-
-  process.stdin('data', (count) =>{)})
-  if count = 4 
-  
-  if (code === 12345) {start()}
-  
-  console.log ('Success! welcome'/ ${userName} 'to city hall on all hallows eve')
-  
-  //else console goodbye 
-  //{process.out} 
-  //The first room
-
-  console.log(
-    "See the entrance door to City Hall?\n There is a note there, read that note and respond Immediately\n You will have three three (3!) tries to get it correctly\n \n \nThe note sitting on a key reads\n  \n  \nWelcome",
-    $(answer),
-    "the code is 12345"
-  );
-
-  //process.stdin('data', (count) =>{})
-  //if count = 4
-
-  if (code == 12345) {
-    start();
-  }
-
-  cponsole.log("Success! welcome, ${userName} to city hall on all hallows eve");
-  //else console goodbye
-  //{process.out}
-  //console.log ("Lets go.")
-  //
-  //console.log(` `)
-  console.log(
-    `Ok, ${userName}, Let us begin.\nHead for the front door at City Hall right behind you. HURRY!`
-  );
-
-
-  
-  
-  
-  //console.log("Sorry, I don't know how to  _____");
-
-  //let sign = {
-  //description: "The code to enter is 12345.\nEnter at your own risk. "
-  //read: () => {
-  //return this.description
-  //}
-  //}
-
-  
-
-  //console.log("You can't go that way")
-  //process.exit();
-//}
-
-
-
-
-
-//let orderArr = process.argv.slice(2);
-
-//orderArr.forEach((items) => {
-//orderTotal += menu[items];
-//});
-  console.log(
-    "There is a note on the door that reads.\n To enter you must input this code"
-  );
-  
-  
-  //useful functions 
-
-  // pickUp and drop functions :push, pull, slice, trim. look in week 2; arrays
-//pickUp
-process.stdin.once('data', (pickedUp)=>{pickedUp.toString()})
-if (pickedUp) {console.log('smart move')}
-}  
-//drop item
-
-function dropIt() {pop()
-  console.log(player.inventory.indexOf())
-  player.inventory.splice (player.inventory.indexOf(dropItem) )
-}
-//process.stdin.once('data', (pickedUp)=>{pickedUp.toString()})
-  //if (player) pickedUp =  true {}
-  
-//drop item
-//function dropIt{dropItem){
-  //console.log(player.invetory.indexOf())
-  //player.inventory.splice (player.inventory.indexOf(dropItem) )
-//}
-
-  let antiRm = ["stairCase", "darkHall", "brightHall", "sign"];
-  let study = ["books", "spiders", "redCarpet", "door", "door"];
-  let sachel = [""];
-
-  //class Room {
-  //constructor(description, inventory, left, right, straight, back);
-  // this.description = description;
-  // this.inventory = inventory;
-  //this.left = left;
-  //this.right = right;
-  //this.straight = straight;
-  //this.back = back;
-
-  //describe(){
-  // return
-  //}
-  //let myRoom = new Room("")
-
-  //console.log(myRoom.describe())
-
-  //let orderArr = process.argv.slice(2);
-
-  //orderArr.forEach((items) => {
-  //orderTotal += menu[items];
-  //});
-
+}   
