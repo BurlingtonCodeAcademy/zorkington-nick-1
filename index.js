32430f67aaaf2fd7d2d01de13a6d4b30d64f0666
Jack Brooks <cyrusandethan@yahoo.com>
2021-03-21 13:05:43 -0400

    items constructor

--- index.js
+++ index.js
@@ -21,11 +21,7 @@ let rooms = {
 };
 let currentRoom = "mainStreet";
 
-
-
-
-
-
+//Class constructors of the rooms 
 class Room {
   constructor(
     name,
@@ -39,7 +35,7 @@ class Room {
   ) {
     this.name = name;
     this.description = description;
-    this.inventory = inventory;
+    this.inventory = [];
     this.left = left;
     this.right = right;
     this.straight = straight;
@@ -48,7 +44,7 @@ class Room {
   }
 }
 
-//
+//Room Constructors
 const mainStreet = new Room(
   "main street",
   "It’s a dark damp night and you are on the road out in front of 182 Main St.\n and you swear you heard a soft whisper.\nYou think maybe that was just in your head. \nYou need to decide, Do you really want to continue.? If yes than make your way over to City Hall across the street.",
@@ -61,7 +57,7 @@ const mainStreet = new Room(
 );
 const cityHall = new Room(
   "city hall",
-  "You walk up to City Hall. In front of you is a very large wooden door with a brass head of a lion as knocker. Behind you is the steps you just came up that funnel you to this massive door. You knock, the door swings open as if it was unlocked waiting for you. You step inside nto what you can describe as the anti-chamber",
+  "You walk up to City Hall. In front of you is a very large wooden door with a brass head of a lion as knocker. Behind you is the steps you just came up that funnel you to this massive door. You knock, the door swings open as if it was unlocked waiting for you. You step inside into what you can describe as the anti-chamber",
   [],
   null,
   null,
@@ -71,7 +67,7 @@ const cityHall = new Room(
 );
 const anteChamber = new Room(
   "ante chamber",
-  "You have entered the Antechamber. Right to the roight nof you is an umbrella stand with a big stick in nthe case.\n There is a massive chandlier in the center of the ceiling. It seems to be ever so slightly blowing in the breeze.\n Meanwhile, you heard the door lock behind you. You say to your self maybe I should take tht stick,\nDo you take the stick or not and leave your umnbrella behind/n You preceed down the only path available to you, a short hallway",
+  "You have entered the Antechamber. Right to the right of you is an umbrella stand with a big stick in the stand.\n There is a massive chandlier in the center of the ceiling. It seems to be ever so slightly blowing in the breeze.\n Meanwhile, you heard the door lock behind you.",
   [],
   null,
   null,
@@ -81,7 +77,7 @@ const anteChamber = new Room(
 );
 const hallWay = new Room(
   "hallway",
-  "You have entered the Entrance Hall, which is short and  has a coat room on your left. You decide to leave your rainjacket behind amd gather the ticket stub for your jacket from this elderly, blue haired women.\nAt the end of the hallway you have three choices where to go.\n To the left is a nice sunny lit hallway with people working in their offices.\n to the right is a dark dreary hallway with cobwebs covering the entrance.\n Straight ahead is a huge marble staircase.\n What way will you choose? \n",
+  "You have entered the Entrance Hall, which is short and has a coat room on your left. You decide to leave your rainjacket behind and gather the ticket stub for your jacket from this elderly, blue haired women.\nAt the end of the hallway you have three choices where to go.\n To the left is a nice sunny lit hallway with people working in their offices.\n to the right is a dark dreary hallway with cobwebs covering the entrance.\n Straight ahead is a huge marble staircase.\n What way will you choose? \n",
   [],
   true,
   true,
@@ -91,7 +87,7 @@ const hallWay = new Room(
 );
 const roomOne = new Room(
   "room one",
-  "At the top of the stairs there is another long hallway with many doors, but there is a door on the left that catches your eye, inside you find the door has lead you into an office with a large desk. On the desk is a letter opening knife, in one corner with two chairs in front.\nIn the opposite corner you see a cart that used to house a mini bar but almost everything has been tipped over and broken.\n A untouched bottle of Scotch is the only thing remaining.",
+  "At the top of the stairs there is another long hallway with many doors, but there is a door on the left that catches your eye, inside you find the door has lead you into an office with a large desk in one corner with two chairs in front. On the desk is an old newspaper.\nIn the opposite corner you see a cart that used to house a mini bar but almost everything has been tipped over and broken.\n A untouched bottle of Scotch is the only thing remaining.",
   [],
   null,
   null,
@@ -101,7 +97,7 @@ const roomOne = new Room(
 );
 const roomTwo = new Room(
   "room two",
-  "Back out in the hallway at the top of the stairs is a door to the right is another office, inside is yet another table and chairs with a mini bar in the corner that has only a bottle of Scotch remaining. On one wall of the office is a open window with a crow sitting on the ledge. All of a sudden the crow squawks and says `leave now or regret it`. So you need to leave that room in a hurry but not before picking up the bottle of Scotch.   ",
+  "Back out in the hallway at the top of the stairs is a door to the right is another office, inside is yet another table and chairs with a mini bar in the corner that has only a bottle of Scotch remaining. On one wall of the office is a open window with a crow sitting on the ledge. All of a sudden the crow squawks and says `leave now or regret it`. So you need to leave that room in a hurry but not before picking up the bottle of Scotch.",
   [],
   null,
   null,
@@ -120,14 +116,15 @@ const roomThree = new Room(
   false
 );
 
+//Room Lookup table 
 let roomLookUp = {
-  mainStreet: mainStreet,
-  cityHall: cityHall,
-  anteChamber: anteChamber,
-  hallWay: hallWay,
-  roomOne: roomOne,
-  roomTwo: roomTwo,
-  roomThree: roomThree,
+  'mainStreet': mainStreet,
+  'cityHall': cityHall,
+  'anteChamber': anteChamber,
+  'hallWay': hallWay,
+  'roomOne': roomOne,
+  'roomTwo': roomTwo,
+  'roomThree': roomThree,
 };
 //tracks if the current room can change to new room
 function changeRoom(newRoom) {
@@ -137,10 +134,37 @@ function changeRoom(newRoom) {
     let roomForTable = roomLookUp[currentRoom];
     console.log(currentRoom);
     console.log(roomForTable.description);
-    console.log(roomForTable.straight);
   } else {
     console.log("doors locked");
   }
+}
+
+
+
+//Class Constructor for items
+class Items {
+  constructor(name, description, takeable) {
+    this.name = name,
+    this.description = description,
+    this.takeable = true
+  }
+}
+
+//Items constructor
+const stick = new Items("stick","Large wooden walking stick in case the path before gets rough", true)
+const scotch = new Items("scotch","An untouched, vintage 75 year old, highland, Single Malt Bottle of Scotch with some dust gathering on it.", true)
+const newspaper = new Items("newspaper", "A Newspaper, October 31st, 1946.", false)
+const rainjacket = new Items("rainjacket", "A slightly, damp, rain jacket from the cold wet outside weather.", true)
+const ticketstub = new Items("ticketstub", "You recive a ticket stub for your rainjacket, also on the back it reads:'Good for 1 Free Bottle of Scotch'", true)
+
+
+//item lookup table
+let itemLookUp = {
+  "stick": stick,
+  "scotch": scotch,
+  "newspaper": newspaper,
+  "rainjacket": rainjacket,
+  "ticketstub": ticketstub,
 }
 
 // Function Setting Up `ask()`
